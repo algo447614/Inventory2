ece856af83016e7c0a6b4682c4b69d74ba94e4bf
tanukitika <gondicka@gmail.com>
2024-06-25 19:44:57 -0400

    Initial Commit

---
+++ Inventory2/Inventory2App.swift
@@ -0,0 +1,17 @@
+//
+//  Inventory2App.swift
+//  Inventory2
+//
+//  Created by A. Gondick on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Inventory2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
